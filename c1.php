1fe1d0aabff710806b812aec541bd00f95d719a0
KristijanGjorevski <kikogjorevski@gmail.com>
2023-03-22 21:57:48 +0100

    prv komit za prv cas

---
+++ c1.php
@@ -0,0 +1,46 @@
+<?php
+
+// obicen primer
+
+$student = 'Kristijan'.' Gjorevski';
+$godini = 30;
+
+
+
+// promenliva so data tip string
+$data_tip = '';
+
+// promenliva so data tip boolean
+$data_tip = false;
+
+// promenliva so data tip integer
+$data_tip = 30;
+
+
+// array 
+$ar1 = array($student,'Alojz','Goran');
+
+
+// echo - predefin
+// echo $student;
+
+// predefinirani funkcii 
+
+// echo '<pre>';
+// print_r($ar1);
+// echo '</pre>';
+
+echo '<br>';
+echo '<br>';
+echo '<br>';
+echo '<br>';
+echo '<br>';
+
+echo '<pre>';
+var_dump($ar1);
+echo '</pre>';
+
+
+?>
+<!-- asdadasd -->
+<!-- <h1>Prv Cas</h1> -->
